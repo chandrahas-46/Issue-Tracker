b14260c3d54ac4ff208b300053e9826137c4f17f
Chandrahas Patel <70541620+chandrahas-46@users.noreply.github.com>
2023-10-09 22:04:17 +0530

    Update issue.routes.js

--- src/routes/issue.routes.js
+++ src/routes/issue.routes.js
@@ -11,8 +11,7 @@ issueRouter.get('/', issueController.home);
 issueRouter.post('/project/create', issueController.createProject);
 issueRouter.get('/project/:id', issueController.getOneProject);
 issueRouter.post('/project/:id', issueController.createIssue);
-
+issueRouter.get('/project/delete/:id', issueController.deleteProject);
 
 // module.exports = router;
 export default issueRouter;
-
